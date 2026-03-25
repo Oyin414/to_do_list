fa78aa3b3e3930552f033d752ff970cbc53b6fbe
Oyin414 <kadirioyin88@gmail.com>
2026-03-25 01:36:47 +0000

    feat:update code to allow all buttons on the page to be interactable

--- src/create/list.js
+++ src/create/list.js
@@ -1,10 +1,11 @@
 import { compareAsc, format } from "date-fns";
 
 class ListItems {
-    constructor(title,priority,dues){
+    constructor(title,priority,dues,info){
         this.title = title
         this.priority = priority
         this.due = format(new Date(),dues)
+        this.info = info
         this.id = crypto.randomUUID()
     }
 }
--- src/user_interaction/logic.js
+++ src/user_interaction/logic.js
@@ -13,6 +13,8 @@ let list = document.querySelector(".task_button")
 function setUpForms() {
 const project = document.querySelector(".project_button")
 const cancel = document.querySelector(".btn_cancel")
+const task = document.querySelector(".task_button")
+const cancelTask = document.querySelector(".task_cancel")
 
 project.addEventListener("click",()=>{
   document.getElementById("myForm").style.display = "block";
@@ -21,6 +23,15 @@ project.addEventListener("click",()=>{
 cancel.addEventListener("click",()=>{
  document.getElementById("myForm").style.display = "none";
 })
+
+task.addEventListener("click",()=>{
+  document.getElementById("form").style.display = "block";
+})
+
+cancelTask.addEventListener("click",()=>{
+ document.getElementById("form").style.display = "none";
+})
+
 }
 
 
